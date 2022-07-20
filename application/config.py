7dd2198f68e259dd280fba7ba0c5e5e098879f2d
Christian Rafael <Crvc1998@gmail.com>
2022-07-20 10:35:41 -0500

    Configure postgresql environment

--- application/config.py
+++ application/config.py
@@ -1,5 +1,18 @@
+import os
+
+
 class Config(object):
     """Base configuration"""
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
+    hostname = os.environ["POSTGRES_HOSTNAME"]
+    port = os.environ["POSTGRES_PORT"]
+    database = os.environ["APPLICATION_DB"]
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
